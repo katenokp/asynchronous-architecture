c6eb085a20f8350ab45c06eb857c98283416d3d4
kate <katenokp.p@gmail.com>
2023-08-13 21:51:14 +0500

    Api Gateway

---
+++ TaskTracker/ApiGateway/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+       .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(options =>
+                     {
+                            options.TokenValidationParameters = new TokenValidationParameters
+                                                                {
+                                                                       ValidateIssuer = false,
+                                                                       ValidateAudience = false,
+                                                                       ValidateLifetime = false,
+                                                                       ValidateIssuerSigningKey = true,
+
+                                                                       // ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                                                                       // ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                                                                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
+                                                                };
+                     });
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.UseOcelot().Wait();
+
+app.Run();
